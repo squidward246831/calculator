539dceb42d35017bd793cafcbf483da3efc43983
Advay Ramakrishnan <advayramakrishnan@Advays-MacBook-Air.local>
2021-01-19 14:19:02 -0600

    lets begin work

--- test.c
+++ test.c
@@ -4,6 +4,18 @@ import time
 import calendar
 import ctypes
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import animation
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+import matplotlib.ticker as ticker
+import matplotlib.animation as animation
+from IPython.display import HTML
 import socket
 import time
 import random
@@ -14,8 +26,7 @@ import os
 from random import seed
 from random import random
 from alive_progress import alive_bar
-import pygame
-from pygame.locals import *
+
 sys.stdout.write("\x1b]2;WELCOME TO THE BEGINNING OF THE END ALSO KNOWN AS TBOTE\x07")
 def modifications():
 	print ("Contact Misfortune or Reaper the script is currently under maitnance")
@@ -180,9 +191,69 @@ def security(sec):
 	except KeyboardInterrupt:
 		print ("\nCTRL-C Pressed")
 		exit()
-
-
-
+def barchartiscool():		
+	df = pd.read_csv('https://gist.githubusercontent.com/johnburnmurdoch/4199dbe55095c3e13de8d5b2e5e5307a/raw/fa018b25c24b7b5f47fd0568937ff6c04e384786/city_populations', 
+                 	usecols=['name', 'group', 'year', 'value'])
+	df.head(3)
+	current_year = 2020
+	dff = (df[df['year'].eq(current_year)]
+       	.sort_values(by='value', ascending=True)
+       	.head(10))
+	dff
+	fig, ax = plt.subplots(figsize=(15, 8))
+	ax.barh(dff['name'], dff['value'])
+	colors = dict(zip(
+    	['India', 'Europe', 'Asia', 'Latin America',
+    	 'Middle East', 'North America', 'Africa'],
+    	['#adb0ff', '#ffb3ff', '#90d595', '#e48381',
+     	'#aafbff', '#f7bb5f', '#eafb50']
+	))
+	group_lk = df.set_index('name')['group'].to_dict()
+	fig, ax = plt.subplots(figsize=(15, 8))
+	dff = dff[::-1]   # flip values from top to bottom
+	# pass colors values to `color=`
+	ax.barh(dff['name'], dff['value'], color=[colors[group_lk[x]] for x in dff['name']])
+	# iterate over the values to plot labels and values (Tokyo, Asia, 38194.2)
+	for i, (value, name) in enumerate(zip(dff['value'], dff['name'])):
+  	  ax.text(value, i,     name,            ha='right')  # Tokyo: name
+  	  ax.text(value, i-.25, group_lk[name],  ha='right')  # Asia: group name
+  	  ax.text(value, i,     value,           ha='left')   # 38194.2: value
+	# Add year right middle portion of canvas
+	ax.text(1, 0.4, current_year, transform=ax.transAxes, size=46, ha='right')
+	fig, ax = plt.subplots(figsize=(15, 8))
+	def draw_barchart(year):
+    	dff = df[df['year'].eq(year)].sort_values(by='value', ascending=True).tail(10)
+    	ax.clear()
+    	ax.barh(dff['name'], dff['value'], color=[colors[group_lk[x]] for x in dff['name']])
+   		dx = dff['value'].max() / 200
+	 	for i, (value, name) in enumerate(zip(dff['value'], dff['name'])):
+        	ax.text(value-dx, i,     name,           size=14, weight=600, ha='right', va='bottom')
+        	ax.text(value-dx, i-.25, group_lk[name], size=10, color='#444444', ha='right', va='baseline')
+       		ax.text(value+dx, i,     f'{value:,.0f}',  size=14, ha='left',  va='center')
+    	# ... polished styles
+    	ax.text(1, 0.4, year, transform=ax.transAxes, color='#777777', size=46, ha='right', weight=800)
+    	ax.text(0, 1.06, 'Population (thousands)', transform=ax.transAxes, size=12, color='#777777')
+    	ax.xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+   	 	ax.xaxis.set_ticks_position('top')
+   		 ax.tick_params(axis='x', colors='#777777', labelsize=12)
+   		 ax.set_yticks([])
+   		 ax.margins(0, 0.01)
+   		 ax.grid(which='major', axis='x', linestyle='-')
+   		 ax.set_axisbelow(True)
+   		 ax.text(0, 1.12, 'The most populous cities in the world from 1500 to 2018',
+           	 transform=ax.transAxes, size=24, weight=600, ha='left')
+    	ax.text(1, 0, 'by @pratapvardhan; credit @jburnmurdoch', transform=ax.transAxes, ha='right',
+           	 color='#777777', bbox=dict(facecolor='white', alpha=0.8, edgecolor='white'))
+   	 plt.box(False)
+    
+	draw_barchart(2020)
+	fig, ax = plt.subplots(figsize=(15, 8))
+	animator = animation.FuncAnimation(fig, draw_barchart, frames=range(1968, 2019))
+	HTML(animator.to_jshtml()) 
+	# or use animator.to_html5_video() or animator.save()
+	with plt.xkcd():
+   		fig, ax = plt.subplots(figsize=(15, 8))
+    	draw_barchart(2020)
 def tbote():
 	print("WELCOME TO THE BEGINING OF THE END...")
 	time.sleep(5)
@@ -221,6 +292,8 @@ def compute():
     for i in range(15):
         time.sleep(.1)  # some processing here
         yield  # insert these
+
+
 def ar246831():
 	print("WELCOME TO SPECIAL MODE")
 	sin = input("\033[1;00m[\033[91mSPECIAL MODE\033[1;00m]\033[91m---!>\033[00m ").lower()
@@ -322,7 +395,7 @@ def stopwatch():
 	print('Press ENTER to begin. Afterwards, press ENTER to "click" the stopwatch. Press Ctrl-C to quit.')
 	input()                    # press Enter to begin
 	print('Started.')
-	startTime = time.time()    # get the first lap's start time
+	startTime = time.time()    # get the first laps start time
 	lastTime = startTime
 	lapNum = 1
 	try:
@@ -338,6 +411,7 @@ def stopwatch():
 	except KeyboardInterrupt:
        # Handle the Ctrl-C exception to keep its error message from displaying.
 	    print('\nDone.')
+
 
 
 
@@ -411,6 +485,8 @@ def main():
 		elif sinput == "tools":
 			print (tools)
 			main()
+		elif sinput == "something":
+			somethingasd()
 		elif sinput == "246831":
 			ar246831()
 		elif sinput == "enter":
